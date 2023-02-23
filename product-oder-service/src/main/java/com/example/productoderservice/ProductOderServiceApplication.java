c0e8fbbb69ce60f79c578c9b1721c0a5d8d44148
SuperHamzzi <gksrnrrl6gh@naver.com>
2023-02-23 22:33:28 +0900

    2-23 commit

---
+++ product-oder-service/src/main/java/com/example/productoderservice/ProductOderServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.productoderservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductOderServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProductOderServiceApplication.class, args);
+    }
+
+}
---
+++ product-oder-service/src/test/java/com/example/productoderservice/ProductOderServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.productoderservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductOderServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
